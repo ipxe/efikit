51368c753f2ab75b532e27161e93605ab737d396
Michael Brown <mbrown@fensystems.co.uk>
2020-08-20 16:11:19 +0100

    test: Add HDD and URI device path test cases
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- src/efidevpathtest.c
+++ src/efidevpathtest.c
@@ -27,6 +27,35 @@
 /** PCI root ACPI HID */
 #define HID_PCIROOT EISA_PNP_ID ( 0x0a03 )
 
+/** Sample hard disk device path text */
+static const char *hddpath_text =
+	"PciRoot(0x0)/Pci(0x1,0x1)/Ata(0x0)";
+
+/** Sample hard disk device path */
+static const struct {
+	ACPI_HID_DEVICE_PATH pciroot;
+	PCI_DEVICE_PATH pci;
+	ATAPI_DEVICE_PATH atapi;
+	EFI_DEVICE_PATH_PROTOCOL end;
+} __attribute__ (( packed )) hddpath = {
+	.pciroot = {
+		.Header = DPHDR ( ACPI_DEVICE_PATH, ACPI_DP,
+				  sizeof ( hddpath.pciroot ) ),
+		.HID = HID_PCIROOT,
+	},
+	.pci = {
+		.Header = DPHDR ( HARDWARE_DEVICE_PATH, HW_PCI_DP,
+				  sizeof ( hddpath.pci ) ),
+		.Function = 0x1,
+		.Device = 0x1,
+	},
+	.atapi = {
+		.Header = DPHDR ( MESSAGING_DEVICE_PATH, MSG_ATAPI_DP,
+				  sizeof ( hddpath.atapi ) ),
+	},
+	.end = DPEND,
+};
+
 /** Sample MAC device path text */
 static const char *macpath_text =
 	"PciRoot(0x0)/Pci(0x3,0x0)/MAC(525400123456,0x1)";
@@ -55,6 +84,64 @@ static const struct {
 		.MacAddress = { { 0x52, 0x54, 0x00, 0x12, 0x34, 0x56, } },
 		.IfType = 0x01,
 	},
+	.end = DPEND,
+};
+
+/** Sample URI device path text */
+static const char *uripath_text =
+	"PciRoot(0x0)/Pci(0x1C,0x2)/Pci(0x0,0x1)/MAC(525400AC9C41,0x1)/"
+	"IPv4(0.0.0.0)/Uri(http://boot.ipxe.org/ipxe.efi)";
+
+/** Sample URI device path text (unshortened) */
+static const char *uripath_text_long =
+	"PciRoot(0x0)/Pci(0x1C,0x2)/Pci(0x0,0x1)/MAC(525400AC9C41,0x1)/"
+	"IPv4(0.0.0.0,0x0,DHCP,0.0.0.0,0.0.0.0,0.0.0.0)/"
+	"Uri(http://boot.ipxe.org/ipxe.efi)";
+
+/** Sample URI device path */
+static const struct {
+	ACPI_HID_DEVICE_PATH pciroot;
+	PCI_DEVICE_PATH pci1;
+	PCI_DEVICE_PATH pci2;
+	MAC_ADDR_DEVICE_PATH mac;
+	IPv4_DEVICE_PATH ipv4;
+	URI_DEVICE_PATH uri;
+	char uri_text[29];
+	EFI_DEVICE_PATH_PROTOCOL end;
+} __attribute__ (( packed )) uripath = {
+	.pciroot = {
+		.Header = DPHDR ( ACPI_DEVICE_PATH, ACPI_DP,
+				  sizeof ( uripath.pciroot ) ),
+		.HID = HID_PCIROOT,
+	},
+	.pci1 = {
+		.Header = DPHDR ( HARDWARE_DEVICE_PATH, HW_PCI_DP,
+				  sizeof ( uripath.pci1 ) ),
+		.Function = 0x2,
+		.Device = 0x1c,
+	},
+	.pci2 = {
+		.Header = DPHDR ( HARDWARE_DEVICE_PATH, HW_PCI_DP,
+				  sizeof ( uripath.pci2 ) ),
+		.Function = 0x1,
+		.Device = 0x0,
+	},
+	.mac = {
+		.Header = DPHDR ( MESSAGING_DEVICE_PATH, MSG_MAC_ADDR_DP,
+				  sizeof ( uripath.mac ) ),
+		.MacAddress = { { 0x52, 0x54, 0x00, 0xac, 0x9c, 0x41, } },
+		.IfType = 0x01,
+	},
+	.ipv4 = {
+		.Header = DPHDR ( MESSAGING_DEVICE_PATH, MSG_IPv4_DP,
+				  sizeof ( uripath.ipv4 ) ),
+	},
+	.uri = {
+		.Header = DPHDR ( MESSAGING_DEVICE_PATH, MSG_URI_DP,
+				  ( sizeof ( uripath.uri ) +
+				    sizeof ( uripath.uri_text ) ) ),
+	},
+	.uri_text = "http://boot.ipxe.org/ipxe.efi",
 	.end = DPEND,
 };
 
@@ -126,8 +213,14 @@ static void assert_to_from_text ( const EFI_DEVICE_PATH_PROTOCOL *path,
 /** Test MAC device path */
 static void test_macpath ( void **state ) {
 	( void ) state;
+	assert_to_from_text ( &hddpath.pciroot.Header, true, true,
+			      hddpath_text );
 	assert_to_from_text ( &macpath.pciroot.Header, false, false,
 			      macpath_text );
+	assert_to_from_text ( &uripath.pciroot.Header, true, true,
+			      uripath_text );
+	assert_to_from_text ( &uripath.pciroot.Header, false, false,
+			      uripath_text_long );
 }
 
 /** Tests */
