72a395f3d23526912b52c494fca3c3fed81b6b79
Michael Brown <mbrown@fensystems.co.uk>
2020-08-24 12:15:47 +0100

    test: Split device path types into individual test cases
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- src/efidevpathtest.c
+++ src/efidevpathtest.c
@@ -210,13 +210,23 @@ static void assert_to_from_text ( const EFI_DEVICE_PATH_PROTOCOL *path,
 	assert_from_text ( text, path );
 }
 
-/** Test MAC device path */
-static void test_macpath ( void **state ) {
+/** Test hard disk device path */
+static void test_hddpath ( void **state ) {
 	( void ) state;
 	assert_to_from_text ( &hddpath.pciroot.Header, true, true,
 			      hddpath_text );
+}
+
+/** Test MAC device path */
+static void test_macpath ( void **state ) {
+	( void ) state;
 	assert_to_from_text ( &macpath.pciroot.Header, false, false,
 			      macpath_text );
+}
+
+/** Test URI device path */
+static void test_uripath ( void **state ) {
+	( void ) state;
 	assert_to_from_text ( &uripath.pciroot.Header, true, true,
 			      uripath_text );
 	assert_to_from_text ( &uripath.pciroot.Header, false, false,
@@ -225,7 +235,9 @@ static void test_macpath ( void **state ) {
 
 /** Tests */
 static const struct CMUnitTest tests[] = {
+	cmocka_unit_test ( test_hddpath ),
 	cmocka_unit_test ( test_macpath ),
+	cmocka_unit_test ( test_uripath ),
 };
 
 /**
